a5f288c11ef0c3f5a6f915e68dca0877a1d7c940
Groboclown <matt@groboclown.net>
2018-01-26 10:51:51 -0600

    Update headers

--- app/src/main/java/net/groboclown/groborss/BASE64.java
+++ app/src/main/java/net/groboclown/groborss/BASE64.java
@@ -1,7 +1,8 @@
-/**
+/*
  * Sparse rss
  * 
  * Copyright (c) 2010, 2011 Stefan Handschuh
+ * Copyright (c) 2018 Groboclown
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- app/src/main/java/net/groboclown/groborss/handler/SimpleHtmlParser.java
+++ app/src/main/java/net/groboclown/groborss/handler/SimpleHtmlParser.java
@@ -1,4 +1,4 @@
-/**
+/*
  * GroboRSS
  *
  * Copyright (c) 2017 Groboclown
